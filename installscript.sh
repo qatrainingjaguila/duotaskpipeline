b70c4976269e8f73bb85eeef3f29201900183b90
qatrainingjaguila <71394922+qatrainingjaguila@users.noreply.github.com>
2020-10-30 15:03:13 +0000

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-if [ $(dpkg-query -W -f='${Status}' curl 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if ![which curl];
 then
   sudo apt-get install curl;
 fi
-if [ $(dpkg-query -W -f='${Status}' docker 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if ![which docker];
 then
   curl https://get.docker.com | bash
 fi
-if [ $(dpkg-query -W -f='${Status}' docker-compose 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if ![which docker-compose];
 then
   sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 fi
