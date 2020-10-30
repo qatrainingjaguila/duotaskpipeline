31486417c3ac75ed6c286e60adb4c26112b4594c
qatrainingjaguila <71394922+qatrainingjaguila@users.noreply.github.com>
2020-10-30 15:08:12 +0000

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-if ![which curl];
+if ! which curl > /dev/null;
 then
   sudo apt-get install curl;
 fi
-if ![which docker];
+if ! which docker > /dev/null;
 then
   curl https://get.docker.com | bash
 fi
-if ![which docker-compose];
+if ! which docker-compose > /dev/null;
 then
   sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 fi
